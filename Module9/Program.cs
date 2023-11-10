3a07fd61ac2026de3d10bd2d4eee44c115273828
kjk080kjk0@mail.ru <kjk080kjk0@mail.ru>
2023-11-10 08:42:00 +0600

    Добавьте файлы проекта.

---
+++ Module9/Program.cs
@@ -0,0 +1,28 @@
+﻿namespace Program
+{
+    public delegate void DelEve();
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            dee aa = new dee();
+            aa.ede += Caca;
+            aa.Raise();
+            Console.WriteLine("1212");
+        }
+        public static void Caca()
+        {
+            Console.WriteLine("Типо евент");
+        }
+    }
+    class dee
+    {
+        public event DelEve ede;
+        public void Raise()
+        {
+            Thread.Sleep(1000);
+            Console.WriteLine("asdas");
+            ede.Invoke();
+        }
+    }
+}
